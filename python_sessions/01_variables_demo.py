fdfd550008e7700619d9fca11c10816e9b664401
Chandra <cbagade@yahoo.com>
2024-10-26 04:14:16 +0000

    changes

--- python_sessions/01_variables_demo.py
+++ python_sessions/01_variables_demo.py
@@ -14,7 +14,16 @@ For ex
 # string, int, float, boolean variables
 
 
+orange_price = 85.86
+discount = 0.10
+
+
+
 # evluation order and printing
+
+final_proce_after_dis = (1 - discount) * orange_price
+print(f"{final_proce_after_dis:.2f} is final price after discount")
+
 
 
 # limiting decimals
@@ -22,9 +31,15 @@ For ex
 
 # concatenate string
 
+team = "devops"
+greeting_messgae = "Welcome " + team + " team"
+print(f"{greeting_messgae}")
+
 
 
 # upper and lower case
+print(f"{greeting_messgae.upper()}")
+
 
 # string separated by blank spaces with split and getting first and last word
 
