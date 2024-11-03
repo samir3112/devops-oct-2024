785fe8e7c11a398f45a55a8e0cfad73989544198
Chandra <cbagade@yahoo.com>
2024-11-03 04:22:07 +0000

    changes

--- python_sessions/07_if_conditional_demo.py
+++
@@ -1,9 +0,0 @@
-# Evalute comparision to do or to not do something
-# if statement allows to use boolean, to compare
-
-
-# if condition (with elif and else)
-
-
-
-
--- python_sessions/14_rest_apis/02_flask_mongo.py/mongopass.py
+++
@@ -1,1 +0,0 @@
-mongo_conn_string = "mongodb+srv://<username>:<password>@chandra-dev.pcpogvu.mongodb.net/<cluster_name>?retryWrites=true&w=majority"
